aa77284b71b4b4250818a889c69f676513ba79cd
Rasmus Lääne <rasmus.laane@ut.ee>
2021-12-09 20:34:27 +0200

    Esialgne

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.listen(3000);
+
+app.use(express.static('Public'));
+
+app.get('/', (req, res) => {
+res.render('index');
+});
+
+app.get('/index', (req, res) => {
+res.render('index');
+});
+
+app.get('/posts', (req, res) => {
+res.render('posts');
+});
+
+app.get('/addnewpost', (req, res) => {
+res.render('addnewpost');
+});
+
+app.get('/singlepost', (req, res) => {
+res.render('singlepost');
+});
+
+app.use((req, res) => {
+res.status(404).render('404');
+});
---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+res.setHeader('Content-Type', 'text/html');
+let path = './views/'
+switch (req.url) {
+case '/':
+path += 'index.html';
+break;
+case '/posts':
+path += 'posts.html';
+break;
+case '/addnewpost':
+path += 'addnewpost.html';
+break;
+case '/singlepost':
+path += 'singlepost.html';
+break;
+default:
+path += '404.html';
+break;
+}
+fs.readFile(path, (err, data) => {
+if (err) {
+console.log(err);
+} else {
+res.write(data);
+res.end();
+}
+})
+});
+server.listen(3000, 'localhost', () => {
+console.log('I am listing for requests on port 3000');
+console.log('I am still listing for requests on port 3000');
+});
