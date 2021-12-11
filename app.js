40ddeb87d6eba7eea0dd5a4c8201ba56a82c2c06
Janeli <59377174+6unli@users.noreply.github.com>
2021-12-11 18:39:24 +0200

    make posts work with no images

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.get('/posts', (req, res) => {
     let posts = [
         { id:1, name: "Jon Snow", dp:"user.png", date: "Sep 18, 2020 15:16", image: "img1.jpeg", body: "I think it's going to rain", likes: "1440"},
         { id:2, name: "Lorem Ipsum", dp:"wick.png", date: "Feb 22, 2022 22:22", image: "img2.jpeg", body: "I think I'm back", likes: "2022" },
+        { id:3, name: "Dolor sit amet", dp:"", date: "Apr 5, 2022 23:55", image: "", body: "This post does not have any images. Not for content nor for user display picture", likes: "404" },
     ];
     res.render('posts', { posts: posts , title: 'Posts page'});
 });
