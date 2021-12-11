29045978b52af8492f7a9f414b76a36ab1cadd33
Janeli <59377174+6unli@users.noreply.github.com>
2021-12-11 19:16:17 +0200

    create structure of singlepost.ejs

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ app.get('/addnewpost', (req, res) => {
 });
 
 app.get('/singlepost', (req, res) => {
-    res.render('singlepost',{ title: 'Single post'});
+    let posts = [
+        { id:1, name: "Jon Snow", dp:"user.png", date: "Sep 18, 2020 15:16", image: "img1.jpeg", body: "I think it's going to rain", likes: "1440"},
+        { id:2, name: "Lorem Ipsum", dp:"wick.png", date: "Feb 22, 2022 22:22", image: "img2.jpeg", body: "I think I'm back", likes: "2022" },
+        { id:3, name: "Dolor sit amet", dp:"", date: "Apr 5, 2022 23:55", image: "", body: "This post does not have any images. Not for content nor for user display picture", likes: "404" },
+    ];
+    res.render('singlepost', { posts: posts , title: 'Single post'});
 });
 
 app.use((req, res) => {
