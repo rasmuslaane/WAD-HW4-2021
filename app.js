b22635d3d4461966205a061ea97603eff8703a0a
Janeli <59377174+6unli@users.noreply.github.com>
2021-12-11 17:04:26 +0200

    posts page with manual input

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ app.get('/index', (req, res) => {
 });
 
 app.get('/posts', (req, res) => {
-    res.render('posts',{ title: 'Posts page'});
+    let posts = [
+        { id:1, name: "Jon Snow", dp:"user.png", date: "Sep 18, 2020 15:16", image: "img1.jpeg", body: "I think it's going to rain" },
+        { id:2, name: "Lorem Ipsum", dp:"wick.png", date: "Feb 22, 2022 22:22", image: "img2.jpeg", body: "I think I'm back" },
+    ];
+    res.render('posts', { posts: posts , title: 'Posts page'});
 });
 
 app.get('/addnewpost', (req, res) => {
